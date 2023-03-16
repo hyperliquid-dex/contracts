f7d16a171b5439b2e88f28a45efe7efb3477c7eb
jeff <jeff@chameleontrading.com>
2023-03-16 14:15:13 +0000

    write many more unit tests and fix contract bugs that were found

--- Bridge.sol
+++ Bridge.sol
@@ -21,8 +21,20 @@
       The validators on the L1 listen for and sign DepositEvent events emitted by this contract,
       crediting the L1 with the equivalent USDC. No additional work needs to be done on this contract.
 
-    Note that on epoch changes, the L1 will ensure that new signatures are generated for unclaimed withdrawals
+    Signatures:
+      For withdrawals and validator set updates, the signatures are sent to the bridge contract
+      in the same order as the current validator set, i.e. signing validators should be a subsequence
+      of current validators.
+
+    The L1 will ensure the following, though neither is required by the smart contract:
+      1. The order of current validators are ordered in decreasing order of power.
+      2. The validators are unique.
+
+    On epoch changes, the L1 will ensure that new signatures are generated for unclaimed withdrawals
     for any validators that have changed.
+
+    This bridge contract assumes there will be 20-30 validators,
+    so passing the full set of validators as calldata is reasonable on Arbitrum.
 */
 
 pragma solidity ^0.8.9;
@@ -58,31 +70,31 @@ contract Bridge2 is Ownable, Pausable, ReentrancyGuard {
   bytes32 public validatorSetCheckpoint;
   uint256 public epoch;
   uint256 public powerThreshold;
-  uint256 public totalValidatorPower;
+  uint256 public minTotalValidatorPower;
+  // Expose this for convenience because we only store the hash.
+  uint256 public nValidators;
 
   mapping(bytes32 => bool) processedWithdrawals;
 
+  // These events wrap structs because of a quirk of rust client code which parses them.
   event Deposit(DepositEvent e);
   event Withdraw(WithdrawEvent e);
 
   event ValidatorSetUpdatedEvent(uint256 indexed epoch, address[] validators, uint256[] powers);
 
+  // TODO if it saves gas, have the deployer initialize separately so that all function args can be calldata.
+  // However, calldata does not seem to save gas on Arbitrum, so not a big deal for now.
   constructor(
-    uint256 _totalValidatorPower,
-    uint256 _powerThreshold,
+    uint256 _minTotalValidatorPower,
     address[] memory validators,
     uint256[] memory powers,
     address usdcAddress
   ) {
-    require(validators.length == powers.length, "Malformed current validator set");
+    minTotalValidatorPower = _minTotalValidatorPower;
+    powerThreshold = (2 * _minTotalValidatorPower) / 3;
 
-    powerThreshold = _powerThreshold;
-    totalValidatorPower = _totalValidatorPower;
-    require(
-      powerThreshold >= (2 * totalValidatorPower) / 3,
-      "Power threshold should be at least 2/3 of the total validator power."
-    );
     checkNewValidatorPowers(powers);
+    nValidators = validators.length;
 
     ValidatorSet memory validatorSet;
     validatorSet = ValidatorSet(0, validators, powers);
@@ -94,6 +106,11 @@ contract Bridge2 is Ownable, Pausable, ReentrancyGuard {
   }
 
   function makeCheckpoint(ValidatorSet memory validatorSet) private pure returns (bytes32) {
+    require(
+      validatorSet.validators.length == validatorSet.powers.length,
+      "Malformed validator set"
+    );
+
     bytes32 checkpoint = keccak256(
       abi.encode(validatorSet.validators, validatorSet.powers, validatorSet.epoch)
     );
@@ -109,30 +126,26 @@ contract Bridge2 is Ownable, Pausable, ReentrancyGuard {
   function withdraw(
     uint256 usdc,
     uint256 nonce,
-    ValidatorSet memory validatorSet,
-    Signature[] memory signatures
+    ValidatorSet calldata curValidatorSet,
+    address[] calldata signers,
+    Signature[] calldata signatures
   ) external nonReentrant whenNotPaused {
-    require(
-      makeCheckpoint(validatorSet) == validatorSetCheckpoint,
-      "Supplied current validators and powers do not match the current checkpoint."
-    );
-
     // NOTE: this is a temporary workaround because EIP-191 signatures do not match between rust client and solidity.
     // For now we do not care about the overhead with EIP-712 because Arbitrum gas is basically free.
     Agent memory agent = Agent("a", keccak256(abi.encode(msg.sender, usdc, nonce)));
     bytes32 message = hash(agent);
 
-    require(!processedWithdrawals[message], "Already withdrawn.");
+    require(!processedWithdrawals[message], "Already withdrawn");
     processedWithdrawals[message] = true;
 
-    checkValidatorSignatures(message, validatorSet, signatures);
+    checkValidatorSignatures(message, curValidatorSet, signers, signatures);
     usdcToken.transfer(msg.sender, usdc);
 
     emit Withdraw(
       WithdrawEvent({
         user: msg.sender,
         usdc: usdc,
-        validatorSet: validatorSet,
+        validatorSet: curValidatorSet,
         timestamp: blockMillis()
       })
     );
@@ -140,63 +153,77 @@ contract Bridge2 is Ownable, Pausable, ReentrancyGuard {
 
   function checkValidatorSignatures(
     bytes32 message,
-    ValidatorSet memory validatorSet,
+    ValidatorSet memory curValidatorSet, // Current set of all L1 validators
+    address[] memory signers, // Subsequence of the current L1 validators that signed the message
     Signature[] memory signatures
   ) private view {
+    require(
+      makeCheckpoint(curValidatorSet) == validatorSetCheckpoint,
+      "Supplied current validators and powers do not match the current checkpoint"
+    );
+
+    uint256 nSigners = signers.length;
+    require(nSigners > 0, "Signers empty");
+    require(nSigners == signatures.length, "Signatures and signers have different lengths");
+
     uint256 cumulativePower = 0;
-    for (uint256 i = 0; i < validatorSet.validators.length; i++) {
-      require(
-        recoverSigner(message, signatures[i]) == validatorSet.validators[i],
-        "Validator signature does not match."
-      );
-      cumulativePower = cumulativePower + validatorSet.powers[i];
-      if (cumulativePower > powerThreshold) {
-        break;
+    uint256 signerIdx = 0;
+    uint256 end = curValidatorSet.validators.length;
+
+    for (uint256 curValidatorSetIdx = 0; curValidatorSetIdx < end; curValidatorSetIdx++) {
+      address signer = signers[signerIdx];
+      if (signer == curValidatorSet.validators[curValidatorSetIdx]) {
+        uint256 power = curValidatorSet.powers[curValidatorSetIdx];
+        require(
+          recoverSigner(message, signatures[signerIdx]) == signer,
+          "Validator signature does not match"
+        );
+        cumulativePower += power;
+
+        if (cumulativePower >= powerThreshold) {
+          break;
+        }
+
+        signerIdx += 1;
+        if (signerIdx >= nSigners) {
+          break;
+        }
       }
     }
+
     require(
-      cumulativePower > powerThreshold,
-      "Submitted validator set signatures do not have enough power."
+      cumulativePower >= powerThreshold,
+      "Submitted validator set signatures do not have enough power"
     );
   }
 
   function updateValidatorSet(
-    ValidatorSet calldata newValidatorSet,
-    ValidatorSet calldata curValidatorSet,
-    Signature[] calldata signatures
+    ValidatorSet memory newValidatorSet,
+    ValidatorSet memory curValidatorSet,
+    address[] memory signers,
+    Signature[] memory signatures
   ) external whenNotPaused {
     {
       require(
-        curValidatorSet.epoch == epoch,
-        "Current validatorSet epoch supplied doesn't match the current epoch"
+        makeCheckpoint(curValidatorSet) == validatorSetCheckpoint,
+        "Supplied current validators and powers do not match checkpoint"
       );
+
       require(
         newValidatorSet.epoch > curValidatorSet.epoch,
-        "New validatorSet epoch must be greater than the current epoch"
-      );
-
-      require(
-        newValidatorSet.validators.length == newValidatorSet.powers.length,
-        "Malformed new validator set"
-      );
-
-      require(
-        curValidatorSet.validators.length == signatures.length,
-        "Malformed current validator set"
-      );
-
-      require(
-        makeCheckpoint(curValidatorSet) == validatorSetCheckpoint,
-        "Supplied current validators and powers do not match checkpoint."
+        "New validator set epoch must be greater than the current epoch"
       );
     }
 
-    checkNewValidatorPowers(newValidatorSet.powers);
+    uint256 cumulativePower = checkNewValidatorPowers(newValidatorSet.powers);
     bytes32 newCheckpoint = makeCheckpoint(newValidatorSet);
     Agent memory agent = Agent("a", newCheckpoint);
     bytes32 message = hash(agent);
-    checkValidatorSignatures(message, curValidatorSet, signatures);
+    checkValidatorSignatures(message, curValidatorSet, signers, signatures);
     validatorSetCheckpoint = newCheckpoint;
+    epoch = newValidatorSet.epoch;
+    powerThreshold = (2 * cumulativePower) / 3;
+    nValidators = newValidatorSet.validators.length;
 
     emit ValidatorSetUpdatedEvent(
       newValidatorSet.epoch,
@@ -205,25 +232,25 @@ contract Bridge2 is Ownable, Pausable, ReentrancyGuard {
     );
   }
 
-  function checkNewValidatorPowers(uint256[] memory powers) private view {
+  function checkNewValidatorPowers(uint256[] memory powers) private view returns (uint256) {
     uint256 cumulativePower = 0;
     for (uint256 i = 0; i < powers.length; i++) {
       cumulativePower = cumulativePower + powers[i];
-      if (cumulativePower >= totalValidatorPower) {
-        break;
-      }
     }
 
     require(
-      cumulativePower == totalValidatorPower,
-      "Submitted validator powers do not equal totalValidatorPower."
+      cumulativePower >= minTotalValidatorPower,
+      "Submitted validator powers is less than minTotalValidatorPower"
     );
+    return cumulativePower;
   }
 
   function blockMillis() private view returns (uint256) {
     return 1000 * block.timestamp;
   }
 
+  // Ownership will be relinquished on public launch. The owner does not need power to force withdraw,
+  // as that is controlled by the initially skewed distribution of L1 stake.
   function changePowerThreshold(uint256 _powerThreshold) external onlyOwner whenPaused {
     powerThreshold = _powerThreshold;
   }
--- tests/example.rs
+++ tests/example.rs
@@ -1,15 +1,19 @@
 // This test spins up tendermint, abci, and hardhat servers in separate threads.
 //
 // NOTE: This does not compile without all Chameleon Trading rust dependencies.
+//
 // run_bridge_watcher monitors the old bridge that will be phased out
 // run_bridge_watcher2 monitors the current bridge being audited
 // Replicator, HyperAbci, *Action, and other unexplained things relate to the L1,
 // so please take those as black boxes that work.
+//
+// The most relevant tests that isolate the bridge being audited are
+// bridge2_withdrawal_tests and bridge2_update_validator_tests
 
 use crate::prelude::*;
 use crate::{
     abci_state::AbciStateBuilder,
-    bridge2::ValidatorSet,
+    bridge2::{ValidatorSet, WithdrawalVoucher2},
     bridge_watcher::{run as run_bridge_watcher, DepositEvent},
     bridge_watcher2::run as run_bridge_watcher2,
     etherscan::TXS,
@@ -106,10 +110,10 @@ async fn node_test() {
         "{statuses:?}"
     );
 
-    run_bridge_watcher(Chain::Local, Arc::clone(&replicator), Arc::clone(&tx_batcher)).await;
+    run_bridge_watcher(chain, Arc::clone(&replicator), Arc::clone(&tx_batcher)).await;
 
     let amount = 10.0;
-    let owner_usdc_amount = (2. * amount).with_decimals(USDC_ERC20_DECIMALS);
+    let owner_usdc_amount = (5. * amount).with_decimals(USDC_ERC20_DECIMALS);
     let not_owner_eth_client_usdc_amount = (3. * amount).with_decimals(USDC_ERC20_DECIMALS);
 
     chain
@@ -119,11 +123,7 @@ async fn node_test() {
         .unwrap();
     chain
         .usdc_cid()
-        .send(
-            "transfer",
-            (not_owner_eth_client.address(), (3. * amount).with_decimals(USDC_ERC20_DECIMALS)),
-            &owner_eth_client,
-        )
+        .send("transfer", (not_owner_eth_client.address(), not_owner_eth_client_usdc_amount), &owner_eth_client)
         .await
         .unwrap();
     chain
@@ -286,49 +286,20 @@ async fn node_test() {
 
     TXS.lock().clear();
 
+    let start_epoch = bridge2_update_validator_tests().await;
+    bridge2_withdrawal_tests(start_epoch, None).await;
+
     assert!(replicator.bridge2_cloned().is_some());
-
-    run_bridge_watcher2(Chain::Local, Arc::clone(&replicator), Arc::clone(&tx_batcher)).await;
-
-    let cur_validator_set = ValidatorSet { epoch: 0, validators: vec![user], powers: vec![100] };
-    let signing_key = utils::ed25519_signing_key("golden_inputs/priv_validator_key.json");
-    let owner_validator_address = signing_key.verification_key().to_bytes().into();
-    let validator_user = replicator.validator_user(owner_validator_address).unwrap();
-
-    let sol_cur_validator_set = tu::SolValidatorSet::from_validator_set(cur_validator_set.clone());
-    let validator_set_checkpoint: H256 =
-        chain.bridge2_cid().call("validatorSetCheckpoint", (), &owner_eth_client).await.unwrap();
-    assert_eq!(sol_cur_validator_set.hash(), validator_set_checkpoint);
-
-    let cur_epoch = InfraTime::wall_clock_now().to_unix_millis() / 1_000_000;
-    let new_validator_set = ValidatorSet { epoch: cur_epoch, validators: vec![validator_user], powers: vec![100] };
-    let sol_new_validator_set = tu::SolValidatorSet::from_validator_set(new_validator_set.clone());
-
-    let signature =
-        utils::phantom_agent(sol_new_validator_set.hash()).sign_typed_data(chain.eth_chain(), &owner_wallet).unwrap();
-    chain
-        .bridge2_cid()
-        .send("updateValidatorSet", (sol_new_validator_set, sol_cur_validator_set, vec![signature]), &owner_eth_client)
-        .await
-        .unwrap();
+    run_bridge_watcher2(chain, Arc::clone(&replicator), Arc::clone(&tx_batcher)).await;
+    assert_eq!(replicator.validator_user(main_validator()).unwrap(), main_validator_user());
 
     let action = Box::new(OwnerMintTokenForAction { amount: 99, user });
     let signed_action = SignedAction::new(action, &owner_wallet).unwrap();
     tx_batcher.send_signed_action(signed_action).await.unwrap();
 
-    let action = Box::new(TokenDelegateAction { validator: owner_validator_address, amount: 99 });
+    let action = Box::new(TokenDelegateAction { validator: main_validator(), amount: 99 });
     let signed_action = SignedAction::new(action, &owner_wallet).unwrap();
     tx_batcher.send_signed_action(signed_action).await.unwrap();
-
-    chain
-        .usdc_cid()
-        .send(
-            "approve",
-            (chain.bridge2_cid().address(owner_eth_client.chain()), amount.with_decimals(USDC_ERC20_DECIMALS)),
-            &owner_eth_client,
-        )
-        .await
-        .unwrap();
 
     let tx_receipt = chain
         .bridge2_cid()
@@ -346,7 +317,14 @@ async fn node_test() {
     lu::async_sleep(Duration(3.)).await;
     let claimable_withdrawals = replicator.claimable_withdrawals(user);
     assert!(!claimable_withdrawals.is_empty());
-    tu::bridge2_withdrawal(&chain, owner_eth_client, claimable_withdrawals.first().unwrap()).await.unwrap();
+    let withdrawal_voucher = claimable_withdrawals.first().unwrap();
+    warn!("withdrawing from bridge2" => withdrawal_voucher);
+
+    let res = tu::bridge2_withdrawal(&chain, &not_owner_eth_client, withdrawal_voucher.clone()).await;
+    tu::assert_err(res, "Validator signature does not match");
+    tu::bridge2_withdrawal(&chain, &owner_eth_client, withdrawal_voucher.clone()).await.unwrap();
+    let res = tu::bridge2_withdrawal(&chain, &owner_eth_client, withdrawal_voucher.clone()).await;
+    tu::assert_err(res, "Already withdrawn");
 }
 
 async fn spawn_hardhat_node() {
@@ -368,6 +346,293 @@ async fn claim_withdrawal_on_bridge(
 ) -> infra::Result<Receipt> {
     let WithdrawalVoucher { signature, action: WithdrawAction { usd, nonce }, .. } = withdrawal_voucher;
     chain.bridge_cid().send("withdraw", (signature, usd, nonce), client).await
+}
+
+async fn bridge2_update_validator_tests() -> Epoch {
+    let chain = Chain::Local;
+    let eth_chain = chain.eth_chain();
+
+    let eth_client = chain.eth_client(Nickname::Owner).await;
+    let signing_key = SigningKey::from_bytes(&eth_client.key().to_fixed_bytes()).unwrap();
+
+    let wallet0 = Wallet::from(signing_key);
+    let wallet1 = tu::wallet(1);
+    let wallet2 = tu::wallet(2);
+    let wallet3 = tu::wallet(3);
+
+    let user0 = wallet0.address().into();
+    let user1 = wallet1.address().into();
+    let user2 = wallet2.address().into();
+    let user3 = wallet3.address().into();
+
+    warn!("running bridge2_update_validator_tests" => user0, user1, user2, user3);
+
+    let cur_validator_set = ValidatorSet { epoch: 0, validators: vec![user0], powers: vec![100] };
+    let new_validator_set = ValidatorSet { epoch: 1, validators: vec![user1, user2], powers: vec![50, 50] };
+    utils::update_validator_set(&eth_client, chain, &cur_validator_set, &new_validator_set, &[wallet0]).await.unwrap();
+
+    let cur_validator_set = new_validator_set;
+
+    let new_validator_set = ValidatorSet { epoch: 2, validators: vec![user1, user2, user3], powers: vec![50, 25, 25] };
+    utils::update_validator_set(
+        &eth_client,
+        chain,
+        &cur_validator_set,
+        &new_validator_set,
+        &[wallet1.clone(), wallet2.clone()],
+    )
+    .await
+    .unwrap();
+
+    let amount = 10.0;
+    let usd = amount.with_decimals(USDC_ERC20_DECIMALS);
+    let nonce = 0;
+    let hash = utils::keccak((user0.raw(), usd, nonce));
+    let signatures = vec![utils::phantom_agent(hash).sign_typed_data(chain.eth_chain(), &wallet1).unwrap()];
+    let withdrawal_voucher_no_quorum = WithdrawalVoucher2 {
+        usd,
+        nonce,
+        cur_validator_set: new_validator_set.clone(),
+        signers: vec![user1],
+        signatures,
+    };
+
+    let res = tu::bridge2_withdrawal(&chain, &eth_client, withdrawal_voucher_no_quorum).await;
+    tu::assert_err(res, "Submitted validator set signatures do not have enough power");
+
+    chain.usdc_cid().send("approve", (chain.bridge2_cid().address(eth_chain), u64::MAX), &eth_client).await.unwrap();
+    chain.bridge2_cid().send("deposit", usd, &eth_client).await.unwrap();
+
+    let mut signatures = Vec::new();
+    for wallet in [&wallet1, &wallet2, &wallet3] {
+        signatures.push(utils::phantom_agent(hash).sign_typed_data(eth_chain, wallet).unwrap());
+    }
+    let cur_validator_set = new_validator_set.clone();
+    let withdrawal_voucher = WithdrawalVoucher2 {
+        usd,
+        nonce,
+        cur_validator_set: cur_validator_set.clone(),
+        signers: cur_validator_set.validators.clone(),
+        signatures,
+    };
+    tu::bridge2_withdrawal(&chain, &eth_client, withdrawal_voucher).await.unwrap();
+
+    let new_validator_set = ValidatorSet { epoch: 3, validators: vec![user1, user2, user3], powers: vec![50, 25, 25] };
+    let sol_new_validator_set = new_validator_set.into_sol();
+    let sol_cur_validator_set = sol_new_validator_set.clone();
+
+    let sol_new_validator_set_hash = sol_new_validator_set.hash();
+    let mut signatures = Vec::new();
+    for validator in [&wallet1, &wallet2, &wallet3] {
+        signatures.push(
+            utils::phantom_agent(sol_new_validator_set_hash).sign_typed_data(chain.eth_chain(), validator).unwrap(),
+        );
+    }
+    let res = chain
+        .bridge2_cid()
+        .send(
+            "updateValidatorSet",
+            (
+                sol_new_validator_set.clone(),
+                sol_cur_validator_set.clone(),
+                cur_validator_set.signers(),
+                signatures.clone(),
+            ),
+            &eth_client,
+        )
+        .await;
+    tu::assert_err(res, "Supplied current validators and powers do not match checkpoint");
+
+    let cur_validator_set = ValidatorSet { epoch: 2, validators: vec![user1, user2, user3], powers: vec![50, 25, 25] };
+    let cur_signers = cur_validator_set.signers();
+    let sol_cur_validator_set = cur_validator_set.into_sol();
+    let sol_new_validator_set = sol_cur_validator_set.clone();
+    let res = chain
+        .bridge2_cid()
+        .send(
+            "updateValidatorSet",
+            (
+                sol_new_validator_set,
+                sol_cur_validator_set.clone(),
+                sol_cur_validator_set.validators.clone(),
+                signatures.clone(),
+            ),
+            &eth_client,
+        )
+        .await;
+    tu::assert_err(res, "New validator set epoch must be greater than the current epoch");
+
+    let new_validator_set = ValidatorSet { epoch: 3, validators: vec![user1, user2, user3], powers: vec![50, 25, 25] };
+    let sol_new_validator_set = new_validator_set.into_sol();
+    let mut wrong_signatures = Vec::new();
+    for validator in &[&wallet1, &wallet3, &wallet3] {
+        wrong_signatures.push(
+            utils::phantom_agent(sol_new_validator_set_hash).sign_typed_data(chain.eth_chain(), validator).unwrap(),
+        );
+    }
+    let res = chain
+        .bridge2_cid()
+        .send(
+            "updateValidatorSet",
+            (
+                sol_new_validator_set.clone(),
+                sol_cur_validator_set.clone(),
+                sol_cur_validator_set.validators.clone(),
+                wrong_signatures,
+            ),
+            &eth_client,
+        )
+        .await;
+    tu::assert_err(res, "Validator signature does not match");
+
+    let no_quorum_signatures =
+        vec![utils::phantom_agent(sol_new_validator_set_hash).sign_typed_data(chain.eth_chain(), &wallet1).unwrap()];
+    let res = chain
+        .bridge2_cid()
+        .send(
+            "updateValidatorSet",
+            (sol_new_validator_set, sol_cur_validator_set.clone(), vec![user1.raw()], no_quorum_signatures),
+            &eth_client,
+        )
+        .await;
+    tu::assert_err(res, "Submitted validator set signatures do not have enough power");
+
+    let new_validator_set = ValidatorSet { epoch: 3, validators: vec![user1, user2], powers: vec![50, 15] };
+    let sol_new_validator_set = new_validator_set.clone().into_sol();
+    let res = chain
+        .bridge2_cid()
+        .send(
+            "updateValidatorSet",
+            (sol_new_validator_set, sol_cur_validator_set.clone(), cur_signers, signatures),
+            &eth_client,
+        )
+        .await;
+    tu::assert_err(res, "Submitted validator powers is less than minTotalValidatorPower");
+
+    let new_validator_wallets = utils::wallet_range(100);
+    let new_validators_users = new_validator_wallets.iter().map(|wallet| wallet.address().into()).collect();
+    let powers = vec![1_u64; 100];
+
+    let new_validator_set = ValidatorSet { epoch: 3, validators: new_validators_users, powers: powers.clone() };
+    let cur_validator_set = ValidatorSet { epoch: 2, validators: vec![user1, user2, user3], powers: vec![50, 25, 25] };
+    let wallets = [wallet1.clone(), wallet2.clone(), wallet3.clone()];
+    utils::update_validator_set(&eth_client, chain, &cur_validator_set, &new_validator_set, &wallets).await.unwrap();
+
+    let end_epoch = 4;
+    let cur_validator_set = new_validator_set;
+    let new_validator_set = ValidatorSet { epoch: end_epoch, validators: vec![user0], powers: vec![100] };
+    utils::update_validator_set(
+        &eth_client,
+        chain,
+        &cur_validator_set,
+        &new_validator_set,
+        new_validator_wallets.as_slice(),
+    )
+    .await
+    .unwrap();
+
+    end_epoch
+}
+
+async fn bridge2_withdrawal_tests(start_epoch: Epoch, end_epoch: Option<Epoch>) {
+    let chain = Chain::Local;
+    let eth_client = chain.eth_client(Nickname::Owner).await;
+    let signing_key = SigningKey::from_bytes(&eth_client.key().to_fixed_bytes()).unwrap();
+    let wallet0 = Wallet::from(signing_key);
+    let user0: User = wallet0.address().into();
+
+    let powers = vec![25, 1, 10, 10, 10, 6, 3, 2, 1, 88];
+    let wallets = utils::wallet_range(10);
+    let tot_power: u64 = powers.iter().sum();
+
+    let cur_validator_set = ValidatorSet { epoch: start_epoch, validators: vec![user0], powers: vec![100] };
+    let new_epoch = start_epoch + 1;
+    let validators: Vec<_> = wallets.iter().map(|wallet| wallet.address().into()).collect();
+    let new_validator_set = ValidatorSet { epoch: new_epoch, validators: validators.clone(), powers: powers.clone() };
+    utils::update_validator_set(&eth_client, chain, &cur_validator_set, &new_validator_set, &[wallet0]).await.unwrap();
+
+    let quorum_threshold = (2. * tot_power as f64) / 3.;
+    let mut nonce = 0;
+    let usd = 1.0.with_decimals(USDC_ERC20_DECIMALS);
+    chain.bridge2_cid().send("deposit", 30 * usd, &eth_client).await.unwrap();
+
+    let cur_validator_set = ValidatorSet { epoch: new_epoch, validators, powers: powers.clone() };
+    for inds in [
+        Vec::new(),
+        vec![0],
+        vec![3],
+        vec![9],
+        vec![1, 2, 3, 4, 5, 6],
+        vec![0, 3, 6, 8],
+        vec![6, 7],
+        vec![0, 4, 8, 9],
+        vec![0, 1, 2, 5, 6, 9],
+        vec![1, 2, 6],
+        vec![2, 3, 6, 7, 9],
+        vec![0, 1, 4, 5, 9],
+        vec![0, 5, 7, 8],
+        vec![0, 1, 2, 3, 5, 7, 8],
+        vec![1, 3, 6, 9],
+        vec![1, 2, 4, 5, 7, 8, 9],
+        vec![4, 6, 8],
+        vec![0, 1, 2, 4, 5, 6, 9],
+        vec![1, 2, 3, 7, 8, 9],
+        vec![1, 4, 5, 6, 7, 8, 9],
+        vec![0, 2, 3, 4, 5, 6, 7, 8, 9],
+        vec![0, 2, 3, 7, 9],
+        vec![2, 4, 5, 6, 7, 8],
+        vec![0, 1, 3, 5, 6, 7, 9],
+        vec![0, 5, 7, 8, 9],
+        vec![0, 2, 4, 5, 7, 9],
+        vec![0, 2, 5],
+        vec![0, 1, 3, 4, 6, 7, 9],
+        vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ] {
+        let mut tot_sample_power = 0;
+        let mut signatures = Vec::new();
+        let mut signers = Vec::new();
+        let mut sample_powers = Vec::new();
+        let hash = utils::keccak((user0.raw(), usd, nonce));
+
+        for &i in &inds {
+            let wallet = &wallets[i];
+            let power = powers[i];
+            signatures.push(utils::phantom_agent(hash).sign_typed_data(chain.eth_chain(), wallet).unwrap());
+            signers.push(wallet.address().into());
+            tot_sample_power += power;
+            sample_powers.push(power);
+        }
+
+        let quorum_reached = tot_sample_power as f64 >= quorum_threshold;
+        warn!("withdrawal fuzz test working on" => inds, quorum_reached);
+
+        let withdrawal_voucher =
+            WithdrawalVoucher2 { usd, nonce, signers, cur_validator_set: cur_validator_set.clone(), signatures };
+        nonce += 1;
+
+        let res = tu::bridge2_withdrawal(&chain, &eth_client, withdrawal_voucher).await;
+        assert_eq!(res.is_ok(), quorum_reached, "inds={inds:?} res={res:?}");
+    }
+
+    // return to state that the node integration test can use
+    let epoch = match end_epoch {
+        Some(epoch) => epoch,
+        None => InfraTime::wall_clock_now().to_unix_millis() / 1_000_000,
+    };
+    let new_validator_set = ValidatorSet { epoch, validators: vec![main_validator_user()], powers: vec![100] };
+    utils::update_validator_set(&eth_client, chain, &cur_validator_set, &new_validator_set, wallets.as_slice())
+        .await
+        .unwrap();
+}
+
+// TODO do not hardcode
+fn main_validator_user() -> User {
+    "0x7662db3a3c4243ad55b7cf230357b1a8af0e15fc".parse().unwrap()
+}
+
+fn main_validator() -> H256 {
+    let signing_key = utils::ed25519_signing_key("golden_inputs/priv_validator_key.json");
+    signing_key.verification_key().to_bytes().into()
 }
 
 lazy_static! {
